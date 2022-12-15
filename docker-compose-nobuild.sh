7512dc72fc12e853542f5632147062e5b915da97
Maksim Koshmin <maksim.koshmin@simbirsoft.com>
2022-12-16 01:34:35 +0600

    .

--- docker-compose-nobuild.sh
+++ docker-compose-nobuild.sh
@@ -2,4 +2,4 @@ docker container stop jenkins
 docker container rm jenkins
 docker compose --env-file ./.env.local build
 docker compose --env-file ./.env.local push
-docker compose --env-file ./.env.local up --no-build
+docker compose --env-file ./.env.local up --no-build -d
